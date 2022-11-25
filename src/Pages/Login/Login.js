63bbd9e33ec2b686def69261843b6f19fb63e70e
atik94 <atikurrahmanatik94@gmail.com>
2022-11-26 00:02:29 +0600

    firebase initial setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,11 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const handleLogin = (data) => {
     console.log(data);
   };
@@ -16,13 +20,26 @@ const Login = () => {
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs" />
+            <input
+              type="text"
+              {...register("email", { required: "Email Address is required" })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.email && <p className="text-red-700">{errors.email?.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+            <input
+              type="password"
+              {...register("password", {
+                required: "Your password is required",
+                minLength: { value: 6, message: "Password must be 6 character or more than longer" },
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.password && <p className="text-red-700">{errors.password?.message}</p>}
             <label className="label">
               <span className="label-text">Forget Password</span>
             </label>
