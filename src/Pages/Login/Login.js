471271dd23e4273b38fd9c79fc6ed0927389534b
atik94 <atikurrahmanatik94@gmail.com>
2022-11-25 23:12:55 +0600

    login page design

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h2>this is login page</h2>
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-8">
+        <h2 className="text-3xl text-center">Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs" />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+            <label className="label">
+              <span className="label-text">Forget Password</span>
+            </label>
+          </div>
+          <input className="btn btn-accent w-full" value="Login" type="submit" />
+        </form>
+        <p>
+          New to Website{" "}
+          <Link className="text-accent" to="/register">
+            Create new account here
+          </Link>
+        </p>
+        <div className="divider"></div>
+        <button className="btn btn-outline w-full">Google signup</button>
+      </div>
     </div>
   );
 };
